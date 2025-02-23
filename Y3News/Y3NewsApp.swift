861bdf3a43c47a88566f3c5d200ed7e6d316e738
Sunny <sundet.kz@gmail.com>
2025-02-24 01:57:42 +0500

    Initial Commit

---
+++ Y3News/Y3NewsApp.swift
@@ -0,0 +1,20 @@
+//
+//  Y3NewsApp.swift
+//  Y3News
+//
+//  Created by Sundet Mukhtar on 24.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Y3NewsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Y3NewsTests/Y3NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Y3NewsTests.swift
+//  Y3NewsTests
+//
+//  Created by Sundet Mukhtar on 24.02.2025.
+//
+
+import Testing
+@testable import Y3News
+
+struct Y3NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
